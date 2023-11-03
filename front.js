cb924baa18c92c10a1c2163a7593be163cceafe8
William Joannes <william.joannes@outlook.com>
2023-11-03 11:02:34 +0100

    ngfeijgnpik

--- front.js
+++ front.js
@@ -1,8 +1,11 @@
 
 const h1 = document.getElementById('year');
 
+const backendURLprefix = 'https://whatyearisit-backend-billou-gazouille.vercel.app/';
+//const backendURLprefix = '';
+//const backendURLprefix = 'http://localhost:3000/';
 
-fetch('https://whatyearisit-backend-billou-gazouille.vercel.app/year')
+fetch(backendURLprefix + 'year/')
     .then(resp => resp.json())
     .then(data => {
         h1.innerHTML = data.year;
